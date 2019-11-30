3ca8012378867380c5256735ac1d2009dfd649f9
Hiroyasu Tanabe <h@cosat.com>
2019-11-30 19:01:17 +0900

    tslint quotemark double

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,14 +5,14 @@
   </div>
 </template>
 
-<script>
+<script lang="ts">
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'home',
+  name: "home",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
