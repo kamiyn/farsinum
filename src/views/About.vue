4402a6cf6813893c03fd00c9f5fe65db84249ba3
Hiroyasu Tanabe <h@cosat.com>
2019-11-30 20:40:18 +0900

    import FarsiNumber.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,14 +1,24 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <v-container>
+      <v-layout text-xs-center wrap>
+        <v-flex xs12>
+          <p class="font-weight-bold mb-3 display-3">فارسی</p>
+        </v-flex>
+      </v-layout>
+    </v-container>
+    <hello-world />
   </div>
 </template>
 
 <script lang="ts">
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "about",
   components: {
-  },
+    HelloWorld,
+  }
 };
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="home">
-    <HelloWorld />
+    <FarsiNumber />
   </div>
 </template>
 
 <script lang="ts">
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import FarsiNumber from "@/components/FarsiNumber.vue";
 
 export default {
   name: "home",
   components: {
-    HelloWorld,
+    FarsiNumber,
   },
 };
 </script>
